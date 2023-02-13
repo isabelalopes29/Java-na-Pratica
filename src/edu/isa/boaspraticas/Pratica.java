8eaeb8667a4b9c78a74e61d2ef6412b2b10e45cc
isabelalopes29 <isabelalopesdasilva29@gmail.com>
2023-02-13 14:25:58 -0300

    praticas de codigo

---
+++ src/edu/isa/boaspraticas/Pratica.java
@@ -0,0 +1,19 @@
+package edu.isa.boaspraticas;
+public class Pratica{
+
+public static void main(String[] args) {
+
+    string primeiroNome = "isabela";
+    string segundoNome = "amanda";
+    string nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    system.out.printl(nomeCompleto);
+    
+}
+ public static string nomeCompleto(string primeiroNome, string segundoNome){
+    retun "Resultado do metodo" + primeiroNome.concat(" ").concat(segundoNome);
+
+ }
+
+
+
+}
